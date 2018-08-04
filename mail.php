914157afa3343321c5cb047dc27b689d897f4520
prajwalchoudhry <prajwalc@umich.edu>
2018-08-04 12:20:54 -0400

    More updates

--- mail.php
+++ mail.php
@@ -6,7 +6,6 @@
     $phone= $_POST["phone"];
     
 
-
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
